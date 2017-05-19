834a1bdf8a802e0981005e5a68ea37b8e3e76c87
Jhon <jhonakabane@gmail.com>
2017-05-19 11:44:34 -0400

    Cambios del primer dia de clases

---
+++ src/main/java/edu/pucmm/programacionweb/Main.java
@@ -0,0 +1,10 @@
+package edu.pucmm.programacionweb;
+
+/**
+ * Created by Jhon on 19/5/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
